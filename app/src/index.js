d470820713035cee54d80b614dd2c754d310195f
poymanov <n.poymanov@gmail.com>
2020-06-16 22:22:19 +0300

    Компоненты вынесены в отдельные файлы

--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Drink Coffee</li>
-            <li>Build Awesome App</li>
-        </ul>
-    );
-};
-
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-    return <input type="text" placeholder="search"/>;
-};
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
     return (
