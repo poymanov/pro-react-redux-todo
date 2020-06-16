033bc17796e57f45e43503a238042c68c3f7d551
poymanov <n.poymanov@gmail.com>
2020-06-16 22:12:46 +0300

    Базовая разметка приложения

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const el = (
+    <div>
+      <h1>My ToDo List</h1>
+        <input type="text" placeholder="" />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(el, document.getElementById('root'));
+
