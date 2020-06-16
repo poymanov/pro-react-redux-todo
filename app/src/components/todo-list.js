84ae78137eee2a1353814ed7c4aa512e87e93837
poymanov <n.poymanov@gmail.com>
2020-06-16 22:32:10 +0300

    Добавление ключей для элементов списка дел

--- app/src/components/todo-list.js
+++ app/src/components/todo-list.js
@@ -3,9 +3,12 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({todos}) => {
     const elements = todos.map((item) => {
+
+        const {id, ...itemProps} = item;
+
         return (
-            <li>
-                <TodoListItem {...item} />
+            <li key={id}>
+                <TodoListItem {...itemProps} />
             </li>
         );
     });
