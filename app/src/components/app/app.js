0521912c92a0041ac707bde3c78dd1e4591fea01
poymanov <n.poymanov@gmail.com>
2020-06-17 19:02:45 +0300

    Добавлены удаление элементов списка дел и заготовка для добавления нового элемента в список

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -1,28 +1,69 @@
-import React from "react";
+import React, {Component} from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from "../item-status-filter";
+import ItemAddForm from "../item-add-form";
 import './app.css';
 
-const App = () => {
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3},
-    ];
+export default class App extends Component {
+    maxId = 100;
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter/>
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3},
+        ],
+    };
+
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1),
+            ];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    addItem = (text) => {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        };
+
+        this.setState(({todoData}) => {
+            const newArray = [
+                ...todoData,
+                newItem
+            ];
+
+            return {
+                todoData: newArray
+            };
+        });
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+                <ItemAddForm onItemAdded={this.addItem} />
             </div>
-
-            <TodoList todos={todoData}/>
-        </div>
-    );
+        );
+    }
 };
 
-export default App;
--- app/src/components/todo-list-item/todo-list-item.js
+++ app/src/components/todo-list-item/todo-list-item.js
@@ -25,7 +25,7 @@ export default class TodoListItem extends Component {
     };
 
     render() {
-        const {label} = this.props;
+        const {label, onDeleted} = this.props;
         const {done, important} = this.state;
 
         let classNames = 'todo-list-item';
@@ -48,14 +48,15 @@ export default class TodoListItem extends Component {
               </span>
 
               <button type="button"
-                  className="btn btn-outline-success btn-sm float-right"
-                  onClick={this.onMarkImportant}
+                      className="btn btn-outline-success btn-sm float-right"
+                      onClick={this.onMarkImportant}
               >
                 <i className="fa fa-exclamation"/>
               </button>
 
               <button type="button"
-                      className="btn btn-outline-danger btn-sm float-right">
+                      className="btn btn-outline-danger btn-sm float-right"
+                      onClick={onDeleted} >
                 <i className="fa fa-trash-o"/>
               </button>
             </span>
